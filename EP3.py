1efcfd29c85d105a2ee559c13d548becb93e143c
Trai <tdg52@scarletmail.rutgers.edu>
2019-10-24 10:47:49 -0400

    created a function including regex and called it

--- EP3.py
+++ EP3.py
@@ -2,7 +2,12 @@ import re
 def success(book):
     if type(book) != str:
         raise TypeError("Not a string")
-    result = re.findall('at', book)
-    for element in book:
-        if len(element) >= 3:
-            
+    result = re.findall('[A-Za-z]*at\\b', book)
+    boxes = []
+    for element in result:
+        if len(element) > 3:
+            boxes.append(element)
+    return boxes
+source = open('auntjo.txt')
+jo = source.read()
+print jo
